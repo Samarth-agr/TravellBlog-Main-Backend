7f9a455afe34b8237308c4c57658159c48e8342d
Samarth-agr <143349851+Samarth-agr@users.noreply.github.com>
2025-03-08 13:35:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ const path = require("path");
 
 // CORS configuration
 app.use(cors({
-  origin: "http://localhost:3000", "https://uncovertheworld.vercel.app/" , "https://uncovertheworld-samarth-agrawals-projects-26540647.vercel.app/"
+  origin: ["http://localhost:3000", 
+           "https://uncovertheworld.vercel.app/" , 
+           "https://uncovertheworld-samarth-agrawals-projects-26540647.vercel.app/"],
   methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed methods
   credentials: true, // Allow credentials like cookies or authorization headers
 }));
